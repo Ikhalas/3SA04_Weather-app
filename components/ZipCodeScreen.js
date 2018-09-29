0f111bd497c756ed84a78504eab9ad48a7b8cf7f
Ikhalas <5910110405@psu.ac.th>
2018-09-29 21:01:28 +0700

    สร้างหน้าจอ UI (สกรีน)  หน้าที่ 2

---
+++ components/ZipCodeScreen.js
@@ -0,0 +1,60 @@
+import { StyleSheet, FlatList, View, Text,TouchableHighlight } from 'react-native';
+import React from 'react';
+    
+
+const availableZipItems = [
+    { place: 'Hatyai', code: '90110' },
+    { place: 'Trang', code: '92000' },
+    { place: 'Chiangmai', code: '50000' },
+    { place: 'Khonkaen', code: '40000' },
+    { place: 'Chonburi', code: '20000' },
+]
+    
+const ZipItem = ({place, code, navigate}) => (
+    <View style={styles.zipItem}>
+    <Text style={styles.zipPlace}>{place}</Text>
+    <Text style={styles.zipCode}>{code}</Text>
+    </View>
+)
+
+const _keyExtractor = item => item.code
+
+
+export default class ZipCodeScreen extends React.Component {
+    static navigationOptions = ({navigation}) => {
+        return {
+            headerTitle: (<Text>Choose a zip code</Text>),
+        }
+    }
+
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View>
+                <FlatList data={availableZipItems} keyExtractor={_keyExtractor} 
+                renderItem={({item}) => <ZipItem {...item} navigate={navigate}/>}/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    zipItem: {
+        paddingTop: 40,
+        flexDirection: 'row',
+        justifyContent:'space-between', 
+
+    },
+    zipPlace: {
+        textAlign:'left', 
+        color:'black', 
+        fontSize: 30 ,
+        paddingLeft: 30,  
+    },
+    zipCode: {
+        textAlign:'right', 
+        color:'red', 
+        fontSize: 30 ,
+        paddingRight: 30 ,
+    },
+});
