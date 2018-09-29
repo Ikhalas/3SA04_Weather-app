eadc473d26d635cbada1efce2c723efbee876163
Ikhalas <5910110405@psu.ac.th>
2018-09-29 21:50:02 +0700

    เพิ่มปุ่มในหน้า WeatherScreen เพื่อให้ลิงค์ไปหน้าอื่นๆ

--- components/WeatherScreen.js
+++ components/WeatherScreen.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Weather from './Weather';
+import { Button } from 'react-native';
 
 export default class WeatherScreen extends React.Component {
     static navigationOptions = ({navigation}) => {
         return {
             headerTitle: (<Text>Weather</Text>),
+            headerRight: (
+                <Button title="Select" onPress={() => navigation.navigate('ZipCode')}/>
+            )
+               
         }
+
     }
     render() {
         const zipCode = this.props.navigation.getParam('zipCode')
--- components/ZipCodeScreen.js
+++ components/ZipCodeScreen.js
@@ -11,11 +11,15 @@ const availableZipItems = [
 ]
     
 const ZipItem = ({place, code, navigate}) => (
+    <TouchableHighlight onPress={() => navigate('Weather', {zipCode: code})}>
     <View style={styles.zipItem}>
     <Text style={styles.zipPlace}>{place}</Text>
     <Text style={styles.zipCode}>{code}</Text>
     </View>
-)
+    </TouchableHighlight>
+   )
+
+
 
 const _keyExtractor = item => item.code
 
@@ -27,6 +31,7 @@ export default class ZipCodeScreen extends React.Component {
         }
     }
 
+    
     render() {
         const { navigate } = this.props.navigation;
         return (
